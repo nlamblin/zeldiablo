1217ebfd13d9322fcd1122961c609cfdfa1bf67e
Nicolas Lamblin <nicolas.lamblin7@ent.univ-lorraine.fr>
2015-05-27 15:08:34 +0200

    Modif classe.

--- src/coo_zeldiablo_ferry75u_thenot5u_meurant1u_lamblin4u/Fantome.java
+++ src/coo_zeldiablo_ferry75u_thenot5u_meurant1u_lamblin4u/Fantome.java
@@ -2,10 +2,5 @@ package coo_zeldiablo_ferry75u_thenot5u_meurant1u_lamblin4u;
 
 public class Fantome extends Monstre{
 
-	@Override
-	public void seDeplacer(int direction) {
-		
-	}
-
 
 }
--- src/coo_zeldiablo_ferry75u_thenot5u_meurant1u_lamblin4u/Orc.java
+++ src/coo_zeldiablo_ferry75u_thenot5u_meurant1u_lamblin4u/Orc.java
@@ -2,10 +2,4 @@ package coo_zeldiablo_ferry75u_thenot5u_meurant1u_lamblin4u;
 
 public class Orc extends Monstre{
 
-	@Override
-	public void seDeplacer(int direction) {
-		
-		
-	}
-
 }
