012310cdc3df5f8dccbaef2aa96bc0ed09211c9e
Raphaël THENOT <raphael.thenot1@etu.univ-lorraine.fr>
2015-05-27 14:53:51 +0200

    Modification de la classe Entite

--- src/coo_zeldiablo_ferry75u_thenot5u_meurant1u_lamblin4u/Entite.java
+++ src/coo_zeldiablo_ferry75u_thenot5u_meurant1u_lamblin4u/Entite.java
@@ -1,5 +1,19 @@
 package coo_zeldiablo_ferry75u_thenot5u_meurant1u_lamblin4u;
 
 public abstract class Entite {
-
+	private int x,y;
+	
+	public abstract int getX();
+	
+	public abstract int getY();
+	
+	public abstract int setX();
+	
+	public abstract int setY();
+	
+	public abstract void seDeplacer();
+	
+	
+	
+	
 }
