f3ff2fb687d87bf7104334d58f464cd960836b58
Raphaël THENOT <raphael.thenot1@etu.univ-lorraine.fr>
2015-05-27 15:29:34 +0200

    Ajout de la methode SeDeplacer()

--- src/coo_zeldiablo_ferry75u_thenot5u_meurant1u_lamblin4u/Entite.java
+++ src/coo_zeldiablo_ferry75u_thenot5u_meurant1u_lamblin4u/Entite.java
@@ -2,33 +2,49 @@ package coo_zeldiablo_ferry75u_thenot5u_meurant1u_lamblin4u;
 
 public abstract class Entite {
 	private int x,y;
-	
+
 	public final static int gauche = 1, haut = 2, droite = 3, bas = 4;
-	
+
 	public Entite(int xParam, int yParam) {
 		x = xParam;
 		y = yParam;
 	}
-	
+
 	public int getX(){
 		return this.x;
 	}
-	
+
 	public  int getY(){
 		return this.y;
 	}
-	
+
 	public void setX(int x1){
-		this.x = x1;
-		
+		this.x += x1;
+
 	}
-	
+
 	public void setY(int y1){
-		this.y = y1;
+		this.y += y1;
 	}
 
 	public void seDeplacer(int direction) {
-		
-	}
+		switch(direction){
+		case 1:
+			setX(-1);
+			break;
 
+		case 2 :
+			setY(+1);
+			break;
+
+		case 3 :
+			setX(+1);
+			break;
+
+		case 4 :
+			setY(-1);
+			break;
+
+		}
+	}
 }
