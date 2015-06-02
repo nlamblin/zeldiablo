7345fe7e42b82e1acac2d625770c5c846a277d63
Quentin FERRY <quentinferry@hotmail.fr>
2015-06-02 11:33:37 +0200

    ah si

--- src/monJeu/Entree.java
+++ src/monJeu/Entree.java
@@ -1,5 +1,9 @@
 package monJeu;
 
 public class Entree implements Case{
-
+	@Override
+	public boolean etreTraversable() {
+		// TODO Auto-generated method stub
+		return true;
+	}
 }
--- src/monJeu/Talisman.java
+++ src/monJeu/Talisman.java
@@ -1,5 +1,9 @@
 package monJeu;
 
 public class Talisman implements Case{
-
+	@Override
+	public boolean etreTraversable() {
+		// TODO Auto-generated method stub
+		return true;
+	}
 }
