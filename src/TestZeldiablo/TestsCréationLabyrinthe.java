55dd01dd7e016628d52e2c4c8bb1a7d4e6e5d7d9
Nina Meurant <nina.meurant5@etu.univ-lorraine.fr>
2015-06-08 09:27:52 +0200

    Réorganisation du répertoire document avec ajout de l'itération du 8 juin

---
+++ src/TestZeldiablo/TestsCréationLabyrinthe.java
@@ -0,0 +1,36 @@
+package TestZeldiablo;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+import jdk.nashorn.internal.ir.annotations.Ignore;
+import monJeu.MoteurJeu;
+import monJeu.Personnage;
+
+import org.junit.Test;
+
+public class TestsCréationLabyrinthe {
+
+	@Test
+	public void testTailleLabyrinthe() {
+		assertEquals("le personnage devrait être de 15,15",15, Personnage.getLimitX());
+		assertEquals("le personnage devrait être de 15,15",15, Personnage.getLimitY());
+
+		
+}
+	
+	@Test
+	public void testPositionPersonnage() {
+		Personnage hero = new Personnage(8,8);
+		assertEquals("le personnage devrait être placé en position 8,8",8,hero.getX());
+		assertEquals("le personnage devrait être placé en position 8,8",8,hero.getY());
+
+	}
+	
+	@Ignore
+	public void testMonstreCaseVide() {
+		fail("Not yet implemented");
+	}
+	
+
+
+}
