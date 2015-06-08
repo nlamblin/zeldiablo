d86c1cb74017b539c046ab793026cd09ed738a66
Nina Meurant <nina.meurant5@etu.univ-lorraine.fr>
2015-06-08 17:28:31 +0200

    Préparation des tests pour le Talisman

---
+++ src/TestZeldiablo/TestsTalisman.java
@@ -0,0 +1,28 @@
+package TestZeldiablo;
+
+import jdk.nashorn.internal.ir.annotations.Ignore;
+
+import org.junit.Test;
+
+public class TestsTalisman {
+
+	@Ignore
+	public void testTalismanCaseVide() {
+
+	}
+
+	@Ignore
+	public void testSituationSurTalisman() {
+
+	}
+	
+	@Ignore
+	public void testPlusSurLePlateauApresRecuperation() {
+
+	}
+	
+	@Ignore
+	public void testMonstreRecupererPasTalisman() {
+
+	}
+}
