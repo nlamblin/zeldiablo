4b24eab59920b31292b81cc2c23003b801d799ee
Nicolas Lamblin <nicolas.lamblin7@ent.univ-lorraine.fr>
2015-06-08 11:16:17 +0200

    Tests des collisions fonctionnels.

--- src/TestZeldiablo/TestCollisions.java
+++ src/TestZeldiablo/TestCollisions.java
@@ -33,23 +33,72 @@ public class TestCollisions {
 	
 	@Test
 	public void testCollisionBas() {
-		MonJeu mj = new MonJeu(new Case[15][15]);
+		Case [][] lc =new Case[16][16];
+		for (int i = 0;i<15;i++){
+			for (int j = 0;j<15;j++){
+				lc [i][j] = new CaseVide();
+			}
+		}
+		lc[15][14] = new Mur();
+		MonJeu mj = new MonJeu(lc);
 		Entite e = new Personnage(14, 14, mj);
+		Commande c = new Commande();
+		c.bas= true;
+		e.seDeplacer(c);
+		assertEquals("Le deplacement X ne doit pas pouvoir se faire",14,e.getX());
+		assertEquals("Le deplacement Y ne doit pas pouvoir se faire",14,e.getY());
 	}
 
 	@Test
 	public void testCollisionDroite() {
-		MonJeu mj = new MonJeu(new Case[15][15]);
+		Case [][] lc =new Case[16][16];
+		for (int i = 0;i<15;i++){
+			for (int j = 0;j<15;j++){
+				lc [i][j] = new CaseVide();
+			}
+		}
+		lc[14][15] = new Mur();
+		MonJeu mj = new MonJeu(lc);
 		Entite e = new Personnage(14, 14, mj);
+		Commande c = new Commande();
+		c.droite= true;
+		e.seDeplacer(c);
+		assertEquals("Le deplacement X ne doit pas pouvoir se faire",14,e.getX());
+		assertEquals("Le deplacement Y ne doit pas pouvoir se faire",14,e.getY());
 	}
 	
 	@Test
 	public void testCollisionGauche() {
-		MonJeu mj = new MonJeu(new Case[15][15]);
+		Case [][] lc =new Case[16][16];
+		for (int i = 0;i<15;i++){
+			for (int j = 0;j<15;j++){
+				lc [i][j] = new CaseVide();
+			}
+		}
+		lc[14][13] = new Mur();
+		MonJeu mj = new MonJeu(lc);
 		Entite e = new Personnage(14, 14, mj);
+		Commande c = new Commande();
+		c.gauche= true;
+		e.seDeplacer(c);
+		assertEquals("Le deplacement X ne doit pas pouvoir se faire",14,e.getX());
+		assertEquals("Le deplacement Y ne doit pas pouvoir se faire",14,e.getY());
 	}
 	
 	public void testCaseVide() {
-		
+		Case [][] lc =new Case[16][16];
+		for (int i = 0;i<15;i++){
+			for (int j = 0;j<15;j++){
+				lc [i][j] = new CaseVide();
+			}
+		}
+		lc[14][15] = new CaseVide();
+		MonJeu mj = new MonJeu(lc);
+		Entite e = new Personnage(14, 14, mj);
+		Commande c = new Commande();
+		c.droite= true;
+		e.seDeplacer(c);
+		assertEquals("Le deplacement X ne doit pas pouvoir se faire",15,e.getX());
+		assertEquals("Le deplacement Y ne doit pas pouvoir se faire",14,e.getY());
 	}
 }
