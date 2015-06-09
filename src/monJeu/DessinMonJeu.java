2a0d929fc3825acb5977f64f241ce967375da620
Nina Meurant <nina.meurant5@etu.univ-lorraine.fr>
2015-06-09 16:10:45 +0200

    modif message de fin

--- src/monJeu/DessinMonJeu.java
+++ src/monJeu/DessinMonJeu.java
@@ -70,7 +70,7 @@ public class DessinMonJeu implements DessinJeu {
 	}
 	private void dessinerFin(BufferedImage im){
 		Graphics2D crayon = (Graphics2D) im.getGraphics();
-		crayon.setFont(new Font(Font.SERIF,Font.PLAIN,100));
+		crayon.setFont(new Font(Font.SERIF,Font.PLAIN,80));
 		crayon.setColor(Color.RED);
 		crayon.drawString("Game over !",0,200);
 	}
