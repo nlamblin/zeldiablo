e7f1a68a850e0c5c265e1668b56380a0ac5fb1ea
Quentin FERRY <quentinferry@hotmail.fr>
2015-06-10 17:06:51 +0200

    A

--- src/monJeu/Talisman.java
+++ src/monJeu/Talisman.java
@@ -2,14 +2,26 @@ package monJeu;
 
 import java.awt.Color;
 import java.awt.Graphics2D;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 public class Talisman extends Case implements Declencheur{
-
+	public Talisman(){
+		try {
+			image = ImageIO.read(new File("../coo_zeldiablo_ferry75u_thenot5u_meurant1u_lamblin4u/src/image/Talisman.png"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 	@Override
 	public void afficher(int i,int j,Graphics2D g) {
 		g.setColor(Color.YELLOW);
 		super.afficher(i, j, g);
 		g.fillRect(i * DessinMonJeu.TAILLE_CASE, j * DessinMonJeu.TAILLE_CASE, DessinMonJeu.TAILLE_CASE,DessinMonJeu.TAILLE_CASE);
+		if (image != null&&champ)g.drawImage(image, null, i*DessinMonJeu.TAILLE_CASE, j*DessinMonJeu.TAILLE_CASE);
 	}
 	@Override
 	public void effetCase(MonJeu j) {
