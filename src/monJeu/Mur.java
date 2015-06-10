2c713886149acb2c72b9b8e25728e53413ab8eec
Quentin FERRY <quentinferry@hotmail.fr>
2015-06-10 16:18:58 +0200

    Ajout des sprites

--- src/monJeu/Mur.java
+++ src/monJeu/Mur.java
@@ -2,9 +2,20 @@ package monJeu;
 
 import java.awt.Color;
 import java.awt.Graphics2D;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 public class Mur extends Case{
-
+	public Mur(){
+		try {
+			image = ImageIO.read(new File("../coo_zeldiablo_ferry75u_thenot5u_meurant1u_lamblin4u/src/image/OrcGauche.png"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 	@Override
 	public boolean etreTraversable() {
 		// TODO Auto-generated method stub
@@ -14,6 +25,7 @@ public class Mur extends Case{
 	public void afficher(int i,int j,Graphics2D g) {
 		g.setColor(Color.DARK_GRAY);
 		super.afficher(i, j, g);
-		g.fillRect(i * DessinMonJeu.TAILLE_CASE, j * DessinMonJeu.TAILLE_CASE, DessinMonJeu.TAILLE_CASE,DessinMonJeu.TAILLE_CASE);
+		if (image != null)g.drawImage(image, null, i, j);
+		else g.fillRect(i * DessinMonJeu.TAILLE_CASE, j * DessinMonJeu.TAILLE_CASE, DessinMonJeu.TAILLE_CASE,DessinMonJeu.TAILLE_CASE);
 	}
 }
