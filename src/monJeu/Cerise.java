c9674c4a6299ed17ef29b1c7130488480efbd7ef
Nicolas Lamblin <nicolas.lamblin7@ent.univ-lorraine.fr>
2015-06-10 16:41:10 +0200

    Modif de cerise.

--- src/monJeu/Cerise.java
+++ src/monJeu/Cerise.java
@@ -1,5 +1,7 @@
 package monJeu;
 
+import java.awt.Color;
+import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -11,8 +13,6 @@ import com.sun.prism.Graphics;
 
 public class Cerise extends Case implements Declencheur {
 	
-	private BufferedImage image;
-	
 	public Cerise() {
 		try {
 			image=ImageIO.read(new File("../coo_zeldiablo_ferry75u_thenot5u_meurant1u_lamblin4u/src/image/CERISE.png"));
@@ -22,14 +22,22 @@ public class Cerise extends Case implements Declencheur {
 		}
 	}
 
+	public boolean etreTraversable() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+	
 	@Override
 	public void effetCase(MonJeu j) {
 		// TODO Auto-generated method stub
-		
+		j.pj.setPv(j.pj.getPv()+10);
 	}
 	
-	public void afficher(Graphics g) {
-		
+	public void afficher(int i, int j, Graphics2D g) {
+		g.setColor(Color.DARK_GRAY);
+		super.afficher(i, j, g);
+		if (image != null)g.drawImage(image, null, i, j);
+		else g.fillRect(i * DessinMonJeu.TAILLE_CASE, j * DessinMonJeu.TAILLE_CASE, DessinMonJeu.TAILLE_CASE,DessinMonJeu.TAILLE_CASE);
 	}
 
 }
